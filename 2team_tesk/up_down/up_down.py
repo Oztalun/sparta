20e5c0f05ca394bf99b8f7d5222932e026a288fe
minseong <jeonminseong070300@gmail.com>
2024-07-03 14:40:33 +0900

    횟수 기록도 있었구나

--- 2team_tesk/up_down/up_down.py
+++ 2team_tesk/up_down/up_down.py
@@ -1,11 +1,15 @@
 import random
 
 
-time = 0
+
 restart = 'y'
+best = -1
 check = ['n', "N", "아니요", "아니", "y", "Y", "네"]  # 다시 할지 안할지 물어볼때 양식 맞는지 비교용
 while restart not in check[0:4]:
+    if best!=-1:
+        print(f'최고기록은 {best}번 입니다.')
     computer = random.randint(1, 100)
+    time = 0
     answer = 0
     while answer!=computer:
         error = True
@@ -25,8 +29,9 @@ while restart not in check[0:4]:
             print("up")
         time+=1
         # print(answer,computer)
-    print(f'{time}번 시도하셨습니다')
-    time=0
+    print(f'정답입니다\n{time}번 시도하셨습니다')
+    if best>time or best==-1:
+        best=time
     restart = input('다시 하시겠습니까? (y/n) : ')
     while restart not in check:
         print('양식에 맞게 입력 해 주세요')
